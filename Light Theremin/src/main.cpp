95d979f02b9ea2e569c0153c087261eb620c3cdd
Jacob Collstrup <jacob@collstrup.dk>
2021-06-07 15:04:31 +0200

    updates

--- Light Theremin/src/main.cpp
+++ Light Theremin/src/main.cpp
@@ -1,9 +1,39 @@
 #include <Arduino.h>
 
-void setup() {
+int sensorValue;
+int sensorLow = 1023;
+int sensorHigh = 0;
+
+const int ledPin = 13;
+
+void setup()
+{
   // put your setup code here, to run once:
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, HIGH);
+  //Serial.begin(9600);
+
+  while (millis() < 5000)
+  {
+    sensorValue = analogRead(A0);
+    if (sensorValue > sensorHigh)
+    {
+      sensorHigh = sensorValue;
+    }
+    if (sensorValue < sensorLow)
+    {
+      sensorLow = sensorValue;
+    }
+  }
+  digitalWrite(ledPin, LOW);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
+  //Serial.println("Calibration ended, in main loop.");
+  sensorValue = analogRead(A0);
+  int pitch = map(sensorValue, sensorLow, sensorHigh, 50, 4000);
+  tone(8, pitch, 20);
+  delay(10);
 }
